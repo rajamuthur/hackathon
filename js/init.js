30aa68619ac35f502fa9ef99aec0ae5c7b8e6ccf
rajamuthur <rajamuthu.raan@gmail.com>
2015-12-16 21:44:20 +0530

    create folder structure

---
+++ js/init.js
@@ -0,0 +1,18 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+function resize(){
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	alert("window width: "+w+", height: "+h);
+}
